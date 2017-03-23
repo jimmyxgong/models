120fd3822064c8e6f8d304696e130be7569520e9
Sebastian Amara <sqamara@ucsd.edu>
2017-03-23 19:00:22 +0000

    run with convnet-like 5 layer with one fully connected

--- tutorials/image/cifar10/cifar10.py
+++ tutorials/image/cifar10/cifar10.py
@@ -202,7 +202,7 @@ def inference(images):
   # conv1
   with tf.variable_scope('conv1') as scope:
     kernel = _variable_with_weight_decay('weights',
-                                         shape=[5, 5, 3, 64],
+                                         shape=[3, 3, 3, 64],
                                          stddev=5e-2,
                                          wd=0.0)
     conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
@@ -210,21 +210,10 @@ def inference(images):
     pre_activation = tf.nn.bias_add(conv, biases)
     net = tf.nn.relu(pre_activation, name=scope.name)
     _activation_summary(net)
-  
-  # dropout
-  net = tf.nn.dropout(net, .75)
 
-  # pool1
-  net = tf.nn.max_pool(net, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
-                         padding='SAME', name='pool1')
-#  # norm1
-#  net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-#                    name='norm1')
-
-  # conv2
   with tf.variable_scope('conv2') as scope:
     kernel = _variable_with_weight_decay('weights',
-                                         shape=[5, 5, 64, 64],
+                                         shape=[3, 3, 64, 64],
                                          stddev=5e-2,
                                          wd=0.0)
     conv = tf.nn.conv2d(net, kernel, [1, 1, 1, 1], padding='SAME')
@@ -232,28 +221,79 @@ def inference(images):
     pre_activation = tf.nn.bias_add(conv, biases)
     net = tf.nn.relu(pre_activation, name=scope.name)
     _activation_summary(net)
-  
-  # dropout
-  net = tf.nn.dropout(net, .75)
 
-
-#  # norm2
-#  net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-#                    name='norm2')
-  # pool2
   net = tf.nn.max_pool(net, ksize=[1, 3, 3, 1],
-                         strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+                         strides=[1, 2, 2, 1], padding='SAME', name=('pool1'))
 
-#  # local3
-#  with tf.variable_scope('local3') as scope:
-#    # Move everything into depth so we can perform a single matrix multiply.
-#    reshape = tf.reshape(net, [FLAGS.batch_size, -1])
-#    dim = reshape.get_shape()[1].value
-#    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
-#                                          stddev=0.04, wd=0.004)
-#    biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
-#    net = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+  with tf.variable_scope('conv3') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[3, 3, 64, 128],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(net, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.1))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    net = tf.nn.relu(pre_activation, name=scope.name)
+    _activation_summary(net)
+
+  with tf.variable_scope('conv4') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[3, 3, 128, 128],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(net, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.1))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    net = tf.nn.relu(pre_activation, name=scope.name)
+    _activation_summary(net)
+
+  with tf.variable_scope('conv5') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[3, 3, 128, 128],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(net, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.1))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    net = tf.nn.relu(pre_activation, name=scope.name)
+    _activation_summary(net)
+
+  net = tf.nn.max_pool(net, ksize=[1, 3, 3, 1],
+                         strides=[1, 2, 2, 1], padding='SAME', name=('pool2'))
+
+  
+
+
+
+#  # conv2
+#  with tf.variable_scope('conv2') as scope:
+#    kernel = _variable_with_weight_decay('weights',
+#                                         shape=[5, 5, 64, 64],
+#                                         stddev=5e-2,
+#                                         wd=0.0)
+#    conv = tf.nn.conv2d(net, kernel, [1, 1, 1, 1], padding='SAME')
+#    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+#    pre_activation = tf.nn.bias_add(conv, biases)
+#    net = tf.nn.relu(pre_activation, name=scope.name)
 #    _activation_summary(net)
+#
+##  # norm2
+##  net = tf.nn.lrn(net, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+##                    name='norm2')
+#  # pool2
+#  net = tf.nn.max_pool(net, ksize=[1, 3, 3, 1],
+#                         strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+  # local3
+  with tf.variable_scope('local3') as scope:
+    # Move everything into depth so we can perform a single matrix multiply.
+    reshape = tf.reshape(net, [FLAGS.batch_size, -1])
+    dim = reshape.get_shape()[1].value
+    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                          stddev=0.04, wd=0.0)
+    biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+    net = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+    _activation_summary(net)
 #
 #  # local4
 #  with tf.variable_scope('local4') as scope:
@@ -269,8 +309,8 @@ def inference(images):
   # and performs the softmax internally for efficiency.
   with tf.variable_scope('softmax_linear') as scope:
 
-    net = tf.reshape(net, [FLAGS.batch_size, -1])
-    dim = net.get_shape()[1].value
+    #net = tf.reshape(net, [FLAGS.batch_size, -1])
+    dim = 384 #net.get_shape()[1].value
     weights = _variable_with_weight_decay('weights', [dim, NUM_CLASSES],
                                           stddev=1/dim, wd=0.0)
 
